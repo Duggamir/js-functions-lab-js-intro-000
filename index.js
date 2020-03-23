bf47dd4e337135daa3bc752a0f662ecae597ee2b
Simon Teo <teosimon78@gmail.com>
2020-03-23 18:46:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,5 +6,10 @@ function happyHolidays()
 
 function happyHolidaysTo(name)
 {
-  return 'Happy holidays, %{name}'
+  return 'Happy holidays, %{name}';
+}
+
+function happyCustomHolidayTo(holiday, name)
+{
+  return 'Happy ${holiday}, ${name}';
 }
